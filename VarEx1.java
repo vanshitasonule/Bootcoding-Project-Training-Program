cab5182e5d1e85ce6a42f7f8093c6872f1ff8428
vanshitasonule <144015395+vanshitasonule@users.noreply.github.com>
2023-09-04 16:58:17 +0530

    Create VarEx1.java
    first program

---
+++ VarEx1.java
@@ -0,0 +1,15 @@
+class VarEx1
+{
+	public static void main(String [] args)
+	{
+		int rollNo;
+		rollNo= 10;
+		int age= 21;
+		double price =105.55;
+		boolean isfree=true;
+
+		System.out.println(age);
+		System.out.println(price);
+		System.out.println(isfree);
+	}
+}
