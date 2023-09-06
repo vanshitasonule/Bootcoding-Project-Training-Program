fc6555f8edc005700bf7b699147b06154eed6bc0
vanshitasonule <144015395+vanshitasonule@users.noreply.github.com>
2023-09-06 16:52:24 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,19 @@
-
+ class palindrome {
+ public static void main(String args[]){
+  int n=1221;
+  int i=n;
+  int rev=0;
+  int number=0;
+  while(n!=0){
+  number=n%10;
+  rev=(rev*10)+number;
+  n=n/10;
+  }
+  if(i==rev){
+  System.out.print("Number is palindrome");
+  }
+  else{
+  System.out.print("Not a palindrome");
+}
+}
+}
